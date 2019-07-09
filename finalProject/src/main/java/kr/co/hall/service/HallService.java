631c60fc05530f2f53ac996384f3b15c8c4408b9
gudwns316 <gudwns316@gmail.com>
2019-07-09 14:24:27 +0900

    검색 후 페이지 처리 진행중

--- finalProject/src/main/java/kr/co/hall/service/HallService.java
+++ finalProject/src/main/java/kr/co/hall/service/HallService.java
@@ -100,19 +100,22 @@ public class HallService {
 				int pageNaviSize = 5;
 				int pageNo = ((reqPage - 1) / pageNaviSize) * pageNaviSize + 1;
 				if(pageNo != 1) {
-					pageNavi += "<a class='pageBtn' href='hall.do?reqPage="+(pageNo-1)+"'>이전</a>";
+					pageNavi += "<a class='pageBtn' href='hallSc.do?reqPage="+(pageNo-1)+"&hallLoc="+h.getHallLoc()+"&hallType="+h.getHallType()
+					+"&menuType="+h.getHallFoodtype()+"&=hallPerson"+person+"&searckHall"+h.getHallName()+"'>이전</a>";
 				}
 				int i = 1;
 				while(!(i++ > pageNaviSize || pageNo > totalPage)) {
 					if(reqPage == pageNo) {
 						pageNavi += "<span class='selectPage'>"+pageNo+"</span>";
 					}else {
-						pageNavi += "<a class='pageBtn' href='/hall.do?reqPage="+pageNo+"'>"+pageNo+"</a>";
+						pageNavi += "<a class='pageBtn' href='/hallSc.do?reqPage="+pageNo+"&hallLoc="+h.getHallLoc()+"&hallType="+h.getHallType()
+						+"&menuType="+h.getHallFoodtype()+"&=hallPerson"+person+"&searckHall"+h.getHallName()+"'>"+pageNo+"</a>";
 					}
 					pageNo++;
 				}
 				if(pageNo <= totalPage) {
-					pageNavi += "<a class='pageBtn' href='/hall.do?reqPage="+pageNo+"'>다음</a>";
+					pageNavi += "<a class='pageBtn' href='/hall.do?reqPage="+pageNo+"&hallLoc="+h.getHallLoc()+"&hallType="+h.getHallType()
+					+"&menuType="+h.getHallFoodtype()+"&=hallPerson"+person+"&searckHall"+h.getHallName()+"'>다음</a>";
 				}
 				HallPage pd = new HallPage(hList, pageNavi, reqPage);
 				return pd;
